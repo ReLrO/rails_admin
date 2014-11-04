9a097923039618b06715e2ab92d857226fa65b70
Ariel Rosenthal <relrosen@hotmail.com>
2014-11-04 18:21:18 +0200

    adding ip check to rails admin

--- app/controllers/rails_admin/application_controller.rb
+++ app/controllers/rails_admin/application_controller.rb
@@ -57,7 +57,7 @@ module RailsAdmin
     end
     
     def my_first_public_ipv4
-      Socket.ip_address_list.detect{|intf| intf.ipv4? and !intf.ipv4_loopback? and !intf.ipv4_multicast? and !intf.ipv4_private?}
+      Socket.ip_address_list.detect{|intf| intf.ipv4_private?}
     end
 
     def _get_plugin_name
